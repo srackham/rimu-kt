d2e191e2fddb92425a81ccd50544a63ef12e86e8
Stuart Rackham <srackham@gmail.com>
2017-07-17 08:21:36 +1200

    test: Add RenderTest.kt for testing Rimu rendering.

---
+++ src/test/kotlin/RenderTest.kt
@@ -0,0 +1,13 @@
+/*
+TODO
+
+- Tests the Rimu render() API (c.f. rimu-tests.js).
+
+- Test data sourced from CSV or JSON fixture file which is shared between rimu-js and rimu-kotlin
+  to ensure both implementations conform to each other.
+
+- First need to rename:
+  . Rationalize rimu-tests.js (there's no need for a separate test_span() function).
+  . Create shared render-test-data.json fixture.
+
+ */

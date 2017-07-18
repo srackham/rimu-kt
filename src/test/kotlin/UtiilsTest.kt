6ec9c41368024ece9e1d8c7d49ee01bbf5086298
Stuart Rackham <srackham@gmail.com>
2017-07-19 10:29:37 +1200

    test: Added utils.kt test class.

---
+++ src/test/kotlin/UtiilsTest.kt
@@ -0,0 +1,30 @@
+import org.junit.Assert.assertEquals
+import org.junit.Rule
+import org.junit.Test
+import org.junit.rules.TemporaryFolder
+import org.rimumarkup.fileToString
+import org.rimumarkup.readResouce
+import org.rimumarkup.stringToFile
+import java.nio.file.Paths
+
+class UtilsTest {
+
+    @Rule
+    @JvmField
+    var tempFolderRule = TemporaryFolder()
+
+    @Test
+    fun readResouceTest() {
+        val text = readResouce("/test.txt") // Throws exception if not found.
+        assertEquals("Test resouce", text)
+    }
+
+    @Test
+    fun stringToFromFile() {
+        // Test file read/write string function.
+        val fileName = Paths.get(tempFolderRule.root.path, "test.txt").toString()
+        stringToFile("Hello World!", fileName)
+        assertEquals("Hello World!", fileToString(fileName))
+    }
+
+}

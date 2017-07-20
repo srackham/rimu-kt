103c8f8dec18326e635ff6ae8475e6d9a3a1bbd8
Stuart Rackham <srackham@gmail.com>
2017-07-20 20:14:49 +1200

    feature: options.kt initial port.

---
+++ src/main/kotlin/org/rimumarkup/options.kt
@@ -0,0 +1,89 @@
+package org.rimumarkup
+
+object Options {
+
+    // Global option values.
+    // These are initialized when the Option object is instaniated.
+    // TODO: safeMode should be enum.
+    var safeMode = 0
+        set(value) {
+            if (safeMode < 0 || safeMode > 15) {
+                throw IllegalArgumentException("Illegal safeMode value")
+            }
+            field = value
+        }
+
+    var htmlReplacement = "<mark>replaced HTML</mark>"
+    var callback: CallbackFunction = doNothing
+
+    data class RenderOptions(
+            var safeMode: Int? = null,
+            var htmlReplacement: String? = null,
+            var reset: Boolean = false,
+            var callback: CallbackFunction? = null
+    )
+
+    // Reset options to default values.
+    // TODO: Is init() really necessary?
+    fun init() {
+        safeMode = 0
+        htmlReplacement = "<mark>replaced HTML</mark>"
+        callback = doNothing
+    }
+
+    // Return true if safeMode is non-zero.
+    fun isSafeModeNz(): Boolean {
+        return safeMode != 0
+    }
+
+    // Return true if Macro Definitions are ignored.
+    fun skipMacroDefs(): Boolean {
+        return safeMode != 0 && (safeMode and 0x8) == 0
+    }
+
+    // Return true if Block Attribute elements are ignored.
+    fun skipBlockAttributes(): Boolean {
+        return safeMode != 0 && (safeMode and 0x4) != 0
+    }
+
+    fun updateOptions(options: RenderOptions) {
+        if (options.reset) Api.init() // Reset takes priority.
+        // Only update specified (non-null) options.
+        safeMode = options.safeMode ?: safeMode
+        htmlReplacement = options.htmlReplacement ?: htmlReplacement
+        callback = options.callback ?: callback
+    }
+
+    // Set named option value.
+    fun setOption(name: String, value: Any) {
+        var option = Options.RenderOptions()
+        when (name) {
+            "safeMode" -> option.safeMode = value as Int
+            "htmlReplacement" -> option.htmlReplacement = value as String
+            "init" -> option.reset = value as Boolean
+            else -> throw IllegalArgumentException("setOption(name)")
+        }
+        updateOptions(option)
+    }
+
+    // Filter HTML based on current safeMode.
+    fun htmlSafeModeFilter(html: String): String {
+        when (safeMode and 0x3) {
+            0 ->   // Raw HTML (default behavior).
+                return html
+            1 ->   // Drop HTML.
+                return ""
+            2 ->   // Replace HTML with 'htmlReplacement' option string.
+                return htmlReplacement
+            3 ->   // Render HTML as text.
+                return replaceSpecialChars(html)
+            else ->
+                return ""
+        }
+    }
+
+    fun errorCallback(message: String) {
+        callback(CallbackMessage(type = "error", text = message))
+    }
+
+}

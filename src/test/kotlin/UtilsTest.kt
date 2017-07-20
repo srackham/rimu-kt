77716b2fabc4dafa5d34442220e20b33987ab125
Stuart Rackham <srackham@gmail.com>
2017-07-20 20:46:30 +1200

    test: utils.ReplaceSpecialChars().

--- src/test/kotlin/UtilsTest.kt
+++ src/test/kotlin/UtilsTest.kt
@@ -4,6 +4,7 @@ import org.junit.Test
 import org.junit.rules.TemporaryFolder
 import org.rimumarkup.fileToString
 import org.rimumarkup.readResouce
+import org.rimumarkup.replaceSpecialChars
 import org.rimumarkup.stringToFile
 import java.nio.file.Paths
 
@@ -25,6 +26,11 @@ class UtilsTest {
         val fileName = Paths.get(tempFolderRule.root.path, "test.txt").toString()
         stringToFile("Hello World!", fileName)
         assertEquals("Hello World!", fileToString(fileName))
+    }
+
+    @Test
+    fun replaceSpecialCharsTest() {
+        assertEquals("&lt;&lt;Hello &amp; goodbye!&gt;&gt;", replaceSpecialChars("<<Hello & goodbye!>>"))
     }
 
 }

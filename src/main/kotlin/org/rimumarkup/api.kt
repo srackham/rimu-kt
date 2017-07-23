9524701fb884ae7fbdeaa66d83d15194c2738c17
Stuart Rackham <srackham@gmail.com>
2017-07-24 10:59:27 +1200

    feature: Ported Api.render()

--- src/main/kotlin/org/rimumarkup/api.kt
+++ src/main/kotlin/org/rimumarkup/api.kt
@@ -26,18 +26,29 @@ object Api {
     // Set API to default state.
     fun init() {
         Options.init()
-// TODO
-//        BlockAttributes.init()
-//        DelimitedBlocks.init()
+        BlockAttributes.init()
+        DelimitedBlocks.init()
 //        Macros.init()
-//        Quotes.init()
-//        Replacements.init()
+        Quotes.init()
+        Replacements.init()
     }
 
     // Render text with current Options state.
     // TODO: This seems to be redundant, it's equivealent to public render(text) with default opts, so this object is Init.
     fun render(text: String): String {
-        return "<p>${text.trim()}</p>"
+        val reader = Io.Reader(text)
+        val writer = Io.Writer()
+        while (!reader.eof()) {
+            reader.skipBlankLines()
+            if (reader.eof()) break
+//            if (LineBlocks.render(reader, writer)) continue
+//            if (Lists.render(reader, writer)) continue
+            if (DelimitedBlocks.render(reader, writer)) continue
+            // This code should never be executed (normal paragraphs should match anything).
+            throw RimucException("unexpected error: no matching delimited block found")
+        }
+        return writer.toString()
+
     }
 
 }

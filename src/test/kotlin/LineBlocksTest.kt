9f7423bb25374d2ea1b642cc657d437edc44cc67
Stuart Rackham <srackham@gmail.com>
2017-07-25 08:44:46 +1200

    test: Add LineBlocksTest.kt

---
+++ src/test/kotlin/LineBlocksTest.kt
@@ -0,0 +1,27 @@
+//TODO: Is this file necessary?
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Ignore
+import org.junit.Test
+import org.rimumarkup.Api
+import org.rimumarkup.Io
+import org.rimumarkup.LineBlocks
+
+class LineBlocksTest {
+
+    @Before
+    fun before() {
+        Api.init()
+    }
+
+    @Test
+    fun renderTest() {
+        val input = "# Test"
+        val reader = Io.Reader(input)
+        val writer = Io.Writer()
+
+        LineBlocks.render(reader, writer)
+        assertEquals("<h1>Test</h1>", writer.toString())
+    }
+}
+

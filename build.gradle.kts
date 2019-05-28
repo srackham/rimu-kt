503d22389200deda4242d56f359104b0f5bff730
Stuart Rackham <srackham@gmail.com>
2019-05-28 15:22:41 +1200

    build: Set bintray dry run option for testing.

--- build.gradle.kts
+++ build.gradle.kts
@@ -64,7 +64,7 @@ bintray {
     setPublications("rimu") // Name of MavenPublication object.
     user = secretProperties["bintray.user"] as String
     key = secretProperties["bintray.apikey"] as String
-    dryRun = false
+    dryRun = true
     publish = false // Manually publish rimu-kt files from the Bintray maven repo browser UI at https://bintray.com/srackham/maven
     override = false // Whether to override version artifacts already published.
     pkg(delegateClosureOf<BintrayExtension.PackageConfig> {
